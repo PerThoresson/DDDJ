872216b2feb8448cd120a99731873dbcd22c8006
PerThoresson <pthoresson@gmail.com>
2017-10-02 21:46:41 +0200

    quick cleanup

--- cdjService/src/main/java/Main.java
+++ cdjService/src/main/java/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        BeatLinkService beatLinkService = new BeatLinkService();
-        beatLinkService.startService();
+        CDJWatcherService cdjWatcherService = new CDJWatcherService();
+        cdjWatcherService.startService();
     }
 }
